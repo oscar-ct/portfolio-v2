77ed1b1415be623c0a78b2f3885a114a3207165b
Oscar Castro <oscar.a.castro818@gmail.com>
2025-03-03 13:56:10 -0600

    update clip path

--- app/components/BlankDivider.tsx
+++ app/components/BlankDivider.tsx
@@ -18,8 +18,10 @@ const BlankDivider = () => {
         <>
             <div
                 ref={container}
-                className='relative flex items-center justify-center py-20 overflow-hidden'
-                style={{clipPath: "polygon(10% 0, 100% 0%, 100% 0%, 0 100%)"}}
+                className='relative flex items-center justify-center py-28 overflow-hidden'
+                // style={{clipPath: "polygon(10% 0, 100% 0%, 100% 0%, 0 100%)"}}
+                style={{clipPath: "polygon(50% 0, 50% 0%, 100% 100%, 0 100%)"}}
+
             >
                 <div className='fixed top-[-10%] left-0 h-[125%] w-full'>
                     <motion.div style={{y}} className='relative w-full h-full'>
