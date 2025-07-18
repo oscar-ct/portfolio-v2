3e48e059f4f4e8501d1bdb6db8e484cf1ddfd21d
Oscar Castro <oscar.a.castro818@gmail.com>
2025-07-18 16:14:47 -0500

    feat: footer text

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <div className={"text-sm font-bold text-center text-slate-100"}>
-            Designed and developed by Oscar Castro
+            <span>Crafted by Oscar Castro using TypeScript and Next.js</span>
         </div>
     );
 };
